055c63391e2b1d6923bb581007de6ac1473df693
Md.Nawab <md78692nawab@gmail.com>
2025-12-28 22:09:34 +0530

    Added TransferFund page class and test class.

---
+++ src/pages/TransferFund.ts
@@ -0,0 +1,47 @@
+import {expect, Locator,Page} from "@playwright/test";
+import { BasePage } from "./BasePage";
+
+export class TransferFund extends BasePage{
+
+    private readonly transferFundLink:Locator;
+    private readonly transferFundTitle:Locator;
+    private readonly insertAmount:Locator;
+    private readonly srcAccountID:Locator;
+    private readonly targetAccountID:Locator;
+    private readonly transferButton:Locator;
+    private readonly successfulTransferMsg:Locator;
+
+
+    constructor(page:Page){
+        super(page);
+        this.transferFundLink=page.getByRole("link",{name:"Transfer Funds"});
+        this.transferFundTitle=page.getByRole('heading', { name: 'Transfer Funds' })
+        this.insertAmount=page.locator("#amount");
+        this.srcAccountID=page.locator("#fromAccountId");
+        this.targetAccountID=page.locator("#toAccountId");
+        this.transferButton=page.locator("[value='Transfer']");
+        this.successfulTransferMsg=page.getByRole('heading', { name: 'Transfer Complete!' });
+    }
+
+    async clickAndVerifyTransferFund():Promise<boolean>{
+        await this.transferFundLink.click();
+       return await this.transferFundTitle.isVisible();
+        //expect(await this.transferFundTitle.textContent()).toContain("Transfer Funds")
+    }
+
+    async fillAmount_AccouontID_Details(amount:string,srcAccId:string,tarAccId:string){
+       await this.insertAmount.fill(amount);
+       await this.srcAccountID.selectOption(srcAccId);
+       await this.targetAccountID.selectOption(tarAccId);
+       console.log(`your details Amount: ${amount},SrcAccountId: ${srcAccId},TarAccountId: ${tarAccId}`);
+    }
+
+    async clickTransferButton(){
+        await this.transferButton.click();
+    }
+
+    async getSucessfullyTransferMsg():Promise<string|null>{
+       return await this.successfulTransferMsg.textContent();
+    }
+
+}
---
+++ tests/TransferFundTest.spec.ts
@@ -0,0 +1,84 @@
+ import {test,expect} from "@playwright/test";
+import { OpenNewAccount } from "../src/pages/OpenNewAccount";
+import { LoginPage } from "../src/pages/LoginPage";
+import { TransferFund } from "../src/pages/TransferFund";
+import { staticData } from "../src/utils/data";
+import { AccountOverview } from "../src/pages/AccountOverview";
+
+
+ test.describe("Transaction between Source and Target Account",()=>{
+    test("Amount Transfer",async ({page})=>{
+
+
+         // ✅ Network log (correct place)
+        page.on("request", req => {
+            if (req.url().includes("openaccount")) {
+                console.log("Open Account request sent");
+            }
+        });
+
+        const loginPage = new LoginPage(page);
+        const opennewaccount=new OpenNewAccount(page);
+        const transferfund=new TransferFund(page);
+        const accountList:Array<string>=new Array<string>();
+        const accountOverview=new AccountOverview(page); 
+
+        await page.goto("/");
+        await page.waitForLoadState("domcontentloaded");
+        await expect(page).toHaveURL(/parabank\/index\.htm/);
+       
+        await loginPage.fillUserName(staticData.userName);
+        await loginPage.fillPassword(staticData.password);
+        await loginPage.clickLoginBtn();
+
+        expect(await loginPage.getUserSuccessLoginMessage())
+            .toContain(staticData.firstName);
+
+        //create 2 account for transfering between source and target.
+        for(let i=1;i<=2;i++){
+        await opennewaccount.clickOpenNewAccountLink();
+        await opennewaccount.selectAccountType("SAVINGS");
+        await opennewaccount.selectAccountID();
+        await opennewaccount.clickOpenAccountButton();
+
+        const getOpenAccountResult =
+            await opennewaccount.getSuccessMessageForAccountOpen();
+
+        for (const message of getOpenAccountResult) {
+            console.log(message);
+        }
+
+        const getNewAccountID =
+            await opennewaccount.getNewAccountIDNumber();
+
+        console.log("New Account ID:", getNewAccountID);
+        expect(getNewAccountID).toBeTruthy();
+        accountList.push(getNewAccountID);
+        
+    }
+
+        console.log(accountList);
+
+        //check the title is visible or not.
+        expect(await transferfund.clickAndVerifyTransferFund()).toBeTruthy();
+        //pass the value inside the Transfer fund page.
+        await transferfund.fillAmount_AccouontID_Details("100",accountList[0],accountList[1]); 
+        //click transfer button.
+        await transferfund.clickTransferButton();
+
+        expect(await transferfund.getSucessfullyTransferMsg()).toContain("Transfer Complete!");
+
+        await accountOverview.openAccountsOverview();
+        expect(await accountOverview.getHeadingText())
+      .toContain("Accounts Overview");
+      
+       const availableBanlanceSrcAccount=await accountOverview.getAvailableBalanceFromTable(accountList[0]);
+       expect(availableBanlanceSrcAccount).toContain("0.00");
+
+       const availableBanlanceTargetAccount=await accountOverview.getAvailableBalanceFromTable(accountList[1]);
+       expect(availableBanlanceTargetAccount).toContain("200");
+
+
+    })
+
+ })
