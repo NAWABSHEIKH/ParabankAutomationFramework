52bd315a1a59c8883d623ac97ab82541c61bc84b
Md.Nawab <md78692nawab@gmail.com>
2026-01-10 15:28:23 +0530

    Added one more test script that navigate to account details sections and verify the user who perform transaction.

--- tests/BillPay.spec.ts
+++ tests/BillPay.spec.ts
@@ -5,10 +5,11 @@ import { AccountOverview } from "../src/pages/AccountOverview";
 import { LoginPage } from "../src/pages/LoginPage";
 import { staticData } from "../src/utils/data";
 import { OpenNewAccount } from "../src/pages/OpenNewAccount";
+import { AccountDetails } from "../src/pages/AccountDetails";
 
 
 test.describe("Bill Pay",()=>{
-    test("Verify transcation happen through bill pay",async({page})=>{
+    test("should deduct balance when bill payment is made from a newly created account",async({page})=>{
       
         // ✅ Network log (correct place)
         page.on("request", req => {
@@ -101,5 +102,108 @@ test.describe("Bill Pay",()=>{
 
     })
 
+
+    test.only("should display correct transaction details after bill payment",async({page})=>{
+
+
+         // ✅ Network log (correct place)
+        page.on("request", req => {
+            if (req.url().includes("openaccount")) {
+                console.log("Open Account request sent");
+            }
+        });
+        const transactionAmount:number=50;
+        const name="Gian";
+        const loginPage=new LoginPage(page);
+        const billPay=new BillPayPage(page);
+        const openNewAccount=new OpenNewAccount(page);
+        const accountOverview=new AccountOverview(page);
+        const accountDetail=new AccountDetails(page);
+
+        await page.goto("/");
+        await page.waitForLoadState("domcontentloaded");
+        await expect(page).toHaveURL(/parabank\/index\.htm/);
+               
+        await loginPage.fillUserName(staticData.userName);
+        await loginPage.fillPassword(staticData.password);
+        await loginPage.clickLoginBtn();
+        
+        expect(await loginPage.getUserSuccessLoginMessage())
+                    .toContain(staticData.firstName);
+
+        await openNewAccount.clickOpenNewAccountLink();
+        await openNewAccount.selectAccountType("SAVINGS");
+        await openNewAccount.selectAccountID();
+        await openNewAccount.clickOpenAccountButton();
+
+        const getOpenAccountResult =
+            await openNewAccount.getSuccessMessageForAccountOpen();
+
+        for (const message of getOpenAccountResult) {
+            console.log(message);
+        }
+
+        const getNewAccountID =
+            await openNewAccount.getNewAccountIDNumber();
+
+ //       console.log("New Account ID:", getNewAccountID);
+        expect(getNewAccountID).toBeTruthy();
+
+
+        // =========
+
+
+         const billPayHeading=await billPay.clickAndVerifyBillPayLink();
+        expect(billPayHeading).toBeTruthy();
+
+        await billPay.payeeInformation(name,"shibuya","Tokyo","112344","Japan","12345");
+        await billPay.verifyPayeeAccountId("112233","112233");
+        await billPay.selectAccountID(getNewAccountID);
+        await billPay.amountToBeTransact(transactionAmount);
+
+        const successfullyClickSendButton=await billPay.clickAndVerifySendPaymentButton();
+        expect(successfullyClickSendButton).toBeTruthy();
+
+
+   //     console.log(await billPay.getSuccesMessage());
+        expect(await billPay.getSuccesMessage()).toContain(name);
+        expect(await billPay.getSuccesMessage()).toContain(transactionAmount.toString());
+        expect(await billPay.getSuccesMessage()).toContain(getNewAccountID);
+
+    //    ==============
+       await accountOverview.openAccountsOverview();
+       expect(await accountOverview.getHeadingText())
+             .toContain("Accounts Overview"); 
+        // 5️⃣ Verify account exists and click it
+    const accountFound =
+      await accountOverview.verifyAndClickAccount(getNewAccountID);
+    expect(accountFound).toBeTruthy();
+
+    expect(await accountDetail.accountNumberTextVisible()).toBeTruthy();
+
+    await accountDetail.getAccountTransactionLink(name);
+
+    const allTransactionDetails:string[]=await accountDetail.getAllTransactionDetailsInfo();
+
+
+    const transactionId:number=Number(allTransactionDetails[0]);
+    const paymentDate:string=(allTransactionDetails[1]);
+    const paymentByPerson:string=(allTransactionDetails[2]);
+    const transactionType:string=(allTransactionDetails[3]);
+    const amountLeft:string=(allTransactionDetails[4]);
+
+
+    // expect(transactionId).toBe(getNewAccountID);
+    expect(paymentByPerson).toContain(name);
+    expect(transactionType).toContain("Debit");
+    expect(amountLeft).toContain(transactionAmount.toString());
+
+    
+    // for(const data of allTransactionDetails){
+    //     console.log(data);
+    // }
+    // await page.pause();
+
+    })
 
 })
